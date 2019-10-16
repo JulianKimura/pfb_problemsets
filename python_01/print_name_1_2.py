d2a7ae78e7923f8b73edaf5fb2ab587fc92610fe
info <info@info18.cshl.edu>
2019-10-16 11:00:11 -0400

    added color activity and animal based on command input

--- python_01/print_name_1_2.py
+++ python_01/print_name_1_2.py
@@ -2,7 +2,17 @@
 import sys
 
 name = sys.argv[1]
+color = sys.argv[2] 
+activity = sys.argv[3]
+animal = sys.argv[4]
 
-print("My name:", name)
+print("My name:", name,"\n"
+"My favorite color:", color,"\n"
+"My favorite activity:", activity,"\n"
+"My favorite animal:", animal)
+
+
+
+
 
 
