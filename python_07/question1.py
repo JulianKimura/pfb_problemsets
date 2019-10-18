87cc22d5924e336ff0fb2e528d35102d4a7d09c1
info <info@info18.cshl.edu>
2019-10-18 16:34:46 -0400

    answers to question 2 as well

--- python_07/question1.py
+++ python_07/question1.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import re
-
-with open('Python_07_nobody.txt', 'r') as poem:
+#this opens an existing file and creates a new file 
+with open('Python_07_nobody.txt', 'r') as poem, open('Patrick.txt', 'w') as patrick :
+	#this converts the contents of the original poem into a string by using .read() and then storing it into a vairable
 	contents = poem.read()		
-	for found in re.finditer(r"(Nobody)", contents):
-		start = found.start(1) + 1
-		print(start)
+	#this then substitutes all instances of Nobody with Patrick
+	new_poem = re.sub(r"(Nobody)", 'Patrick',  contents)
+	#we are writing a new poem into Patrick.txt
+	patrick.write(new_poem)
 
 
