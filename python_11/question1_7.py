8c452273fb8e8a65fc1ecfad82f48d123514b776
info <info@info18.cshl.edu>
2019-10-21 12:11:14 -0400

    includes challenge question

--- python_11/question1_7.py
+++ python_11/question1_7.py
@@ -5,10 +5,13 @@
 class dna_seq(object):
 #here I am using the init command which allows the class to read in user input
 #the format shows the order by which the user should input when the user wants to extract specific things from the class. 
-	def  __init__(self, sequence, gene_name, organism):
-		self.sequence = sequence
-		self.gene_name = gene_name
-		self.organism = organism
+	def  __init__(self, sequence1, gene_name1, organism1, sequence2, gene_name2, organism2):
+		self.sequence1 = sequence1
+		self.gene_name1 = gene_name1
+		self.organism1 = organism1
+		self.sequence2 = sequence2
+		self.gene_name2 = gene_name2
+		self.organism2 = organism2
 #below a method called length is made, where it takes the length of the sequence attribute defined above, then return the output of the length such that you can print it out later as dna_seq_object.length()
 	def length(self):
 		length_of_sequence = len(self.sequence) 
@@ -31,20 +34,33 @@ class dna_seq(object):
 		return GC
 #below we have a method for returning the inputted sequence in a fasta format
 	def fasta_format(self):
-		fasta_dict = {}
 		header = '>'+self.gene_name
 		sequence = self.sequence
 		string = "{}\n{}"
 		final_string = string.format(header, sequence)
 		return final_string
-
+#below we have a method for comparing two different sequence records and returns true if they are the same, false if they are different. sameness is based on name, organism, sequence
+	def seq_similarity(self):
+		gene_information1 = self.sequence1, self.gene_name1, self.organism1
+		gene_information2= self.sequence2, self.gene_name2, self.organism2
+		if gene_information1 == gene_information2:
+			x = 'true'
+		else:
+			x = 'false'
+		return x
+	 
+			
 
 ######here we are creating a class with the sequence, gnee_name, and organism attributes
-sequence = 'ATGC'
-gene_name = 'test_gene'
-organism = 'julian'
+sequence1 = 'ATGC'
+gene_name1 = 'coolguy'
+organism1 = 'julian'
+sequence2 = 'ATGC'
+gene_name2 = 'coolguy'
+organism2 = 'julian'
 
-dna_seq_object = dna_seq(sequence, gene_name, organism)
 
-print(dna_seq_object.fasta_format())
+dna_seq_object = dna_seq(sequence1, gene_name1, organism1, sequence2, gene_name2, organism2)
+
+print(dna_seq_object.seq_similarity())
 
