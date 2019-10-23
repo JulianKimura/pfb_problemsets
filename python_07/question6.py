f3ed05ff5f20acd80e65b7ec0a12ff7eb7778a32
info <info@info18.cshl.edu>
2019-10-22 22:11:26 -0400

    question6

--- python_07/question6.py
+++ python_07/question6.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
-with open('Python_07_Apol.fasta', 'r') as apol_fasta:
-	
+import re
+
+#open the file
+with open('Python_07_ApoI.fasta.txt', 'r') as fasta:
+#for loop through each line in the fasta
+	for line in fasta:
+#eliminate all new line characters
+		line = line.rstrip()
+#in this regex, we are iterating through the line, looking for the particular variable matches that is defined by
+#the regex. here, the brackets isolates a specific and/or statement from the rest of the string.
+		for matches in re.findall(r"[A|G]AATT[C|T]",line): 
+#having print nested within the for loop makes the matches variable print everytime the for loop rins on each line
+			print(matches) 	
