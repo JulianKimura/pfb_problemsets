f8df0dd08fcc7b9788904ae21d16a745ef4fe460
info <info@info18.cshl.edu>
2019-10-24 12:26:53 -0400

    commentary

--- RNAseq_workshop/sequence_to_kmer_list.py
+++ RNAseq_workshop/sequence_to_kmer_list.py
@@ -27,16 +27,25 @@ def sequence_to_kmer_list(sequence, kmer_length):
 
 	kmers_list = list()
 	sequence = sys.argv[1]
+	kmer_length = sys.argv[2]
     ## begin your code
 #this will set up the counter equal to the actual index in the string
 	nt_index = -1
 	for nt in sequence:
 #the counter will start its count from 0
 		nt_index += 1
-		if len(sequence[nt_index : nt_index + int(sys.argv[2])]) == int(sys.argv[2]):
+#if the length of the sequence that we are pulling out to append into our list at the bottom is equal to the length specified, append. if not, dont. this prevents us from adding any kmers that are smaller at the end of the sequence
+		if len(sequence[nt_index : nt_index + int(kmer_length)]) == int(kmer_length):
 #the command below will append a string that is in the interval specified in the second field of input
-			kmers_list.append(sequence[nt_index:nt_index+int(sys.argv[2])])
+			kmers_list.append(sequence[nt_index:nt_index+int(kmer_length)])
 	return kmers_list
+
+'''
+Brian Haas solution:
+	for kmer_start_pos in range(0,len(sequence)-kmer_length):
+		kmer_end_pos = kmer_start_pos + kmer_length
+		kmer_seq = sequence[kmer_start_pos:
+'''
 
 
 
